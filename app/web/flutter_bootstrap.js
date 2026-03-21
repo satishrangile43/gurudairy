0e6dd2588374ff84e0d4e51e62e3f8f66e8ffc14
satishrangile43 <satishrangile143@gmail.com>
2026-03-22 02:59:34 +0530

    papa

--- app/web/flutter_bootstrap.js
+++ app/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2518036588" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4144050557" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
